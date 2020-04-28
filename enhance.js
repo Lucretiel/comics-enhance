8ca5e2d7065059709e4a7f70eed5031af010dc78
Nathan West <Lucretiel@gmail.com>
2020-04-27 21:37:25 -0400

    Detach from window?

--- enhance.js
+++ enhance.js
@@ -1,29 +1,27 @@
-(() => {
-  const createNavigator = (selector, button) => {
-    if (!selector) return;
-    console.log(`Checking navigator via '${selector}' for '${button}'`);
+const createNavigator = (selector, button) => {
+  if (!selector) return;
+  console.log(`Checking navigator via '${selector}' for '${button}'`);
 
-    const element = document.querySelector(selector);
-    if (!element) return;
+  const element = document.querySelector(selector);
+  if (!element) return;
 
-    const target = element.href;
-    if (!target) return;
+  const target = element.href;
+  if (!target) return;
 
-    console.log(`Creating navigator to '${target}'`);
-    document.addEventListener("keyup", (event) => {
-      if (event.key === button) {
-        window.location.assign(target);
-      }
-    });
-  };
+  console.log(`Creating navigator to '${target}'`);
+  document.addEventListener("keyup", (event) => {
+    if (event.key === button) {
+      window.location.assign(target);
+    }
+  });
+};
 
-  const createNextNav = (selector) => createNavigator(selector, "ArrowRight");
-  const createPrevNav = (selector) => createNavigator(selector, "ArrowLeft");
+const createNextNav = (selector) => createNavigator(selector, "ArrowRight");
+const createPrevNav = (selector) => createNavigator(selector, "ArrowLeft");
 
-  window.enhanceComic = ({ comic, next, prev }) => {
-    console.log("Improving your comics experience");
-    document.querySelector(comic).scrollIntoView();
-    createNextNav(next);
-    createPrevNav(prev);
-  };
-})();
+const enhanceComic = ({ comic, next, prev }) => {
+  console.log("Improving your comics experience");
+  document.querySelector(comic).scrollIntoView();
+  createNextNav(next);
+  createPrevNav(prev);
+};
