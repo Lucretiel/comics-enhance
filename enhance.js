b8ef0c83adb15f358bb307a04b6bdfc604d2d3bf
Nathan West <Lucretiel@gmail.com>
2020-04-27 21:30:10 -0400

    Add comic enhancer script

---
+++ enhance.js
@@ -0,0 +1,33 @@
+(() => {
+  const createNavigator = (selector, button) => {
+    if (!selector) return;
+    console.log(`Checking navigator via '${selector}' for '${button}'`);
+
+    const element = document.querySelector(selector);
+    if (!element) return;
+
+    const target = element.href;
+    if (!target) return;
+
+    console.log(`Creating navigator to '${target}'`);
+    document.addEventListener("keyup", (event) => {
+      if (event.key === button) {
+        window.location.assign(target);
+      }
+    });
+  };
+
+  const createNextNav = (selector) => createNavigator(selector, "ArrowRight");
+  const createPrevNav = (selector) => createNavigator(selector, "ArrowLeft");
+
+  window.enhanceComic = ({ comic, next, prev }) => {
+    console.log("Improving your comics experience");
+    document.querySelector(comic).scrollIntoView();
+    if (next) {
+      createNavigator(next, "ArrowRight");
+    }
+    if (prev) {
+      createNavigator(prev, "ArrowLeft");
+    }
+  };
+})();
