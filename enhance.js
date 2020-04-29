82e91df0fc3eac556d4a8bc13706212649282f5d
Nathan West <Lucretiel@gmail.com>
2020-04-29 17:32:31 -0400

    Minor improvements
    - Add better type checking in search
    - Add preventDefault for arrow keys when nav is installed

--- enhance.js
+++ enhance.js
@@ -1,9 +1,19 @@
+const raise = (thing) => {
+  throw thing;
+};
+
 const search = (root, complexSelector) =>
   Array.isArray(complexSelector)
     ? complexSelector.reduce(search, document)
     : typeof complexSelector === "function"
     ? complexSelector(root)
-    : root.querySelector(complexSelector);
+    : typeof complexSelector === "string"
+    ? root.querySelector(complexSelector)
+    : raise(
+        new Error(
+          `Selector must be a array, function, or string; got ${complexSelector}`
+        )
+      );
 
 const createNavigator = (selector, button) => {
   if (!selector) return;
@@ -16,19 +26,24 @@ const createNavigator = (selector, button) => {
   if (!target) return;
 
   console.log(`Creating navigator to '${target}'`);
+
   document.addEventListener("keyup", (event) => {
     if (event.key === button) {
       window.location.assign(target);
+      event.preventDefault();
+    }
+  });
+
+  document.addEventListener("keydown", (event) => {
+    if (event.key === button) {
+      event.preventDefault();
     }
   });
 };
 
-const createNextNav = (selector) => createNavigator(selector, "ArrowRight");
-const createPrevNav = (selector) => createNavigator(selector, "ArrowLeft");
-
 const enhanceComic = ({ comic, next, prev }) => {
   console.log("Improving your comics experience");
   search(document, comic).scrollIntoView();
-  createNextNav(next);
-  createPrevNav(prev);
+  createNavigator(next, "ArrowRight");
+  createNavigator(prev, "ArrowLeft");
 };
